66ba754d3a49d7b7baa1543cfa7700294e0e1245
Amhjadkhan <amjadpogramer@gmail.com>
2023-07-14 20:31:48 +0400

    second-commit

---
+++ photo-gellary/src/Components/data.jsx
@@ -0,0 +1,85 @@
+const item=[
+
+    {
+        id:0,
+        title:"food-item-1",
+        catagory:"food",
+        price:200,
+        img:"https://media.istockphoto.com/id/1169700146/photo/assorted-indian-non-vegetarian-food-recipe-served-in-a-group-includes-chicken-curry-mutton.webp?b=1&s=170667a&w=0&k=20&c=t6MdFXAoB475RALAXaJrVM-sKbUDblqMtPkeRwPDzzQ=",
+        desc:"This is food challange for the people who love it and cjkdjd kdj dkifk didjfj polorrdjjd fklkgk okgkg"
+    },
+    {
+        id:1,
+        title:"food-item-1",
+        catagory:"food",
+        price:200,
+        img:"https://media.istockphoto.com/id/1169700146/photo/assorted-indian-non-vegetarian-food-recipe-served-in-a-group-includes-chicken-curry-mutton.webp?b=1&s=170667a&w=0&k=20&c=t6MdFXAoB475RALAXaJrVM-sKbUDblqMtPkeRwPDzzQ=",
+        desc:"This is food challange for the people who love it and cjkdjd kdj dkifk didjfj polorrdjjd fklkgk okgkg"
+    },
+    {
+        id:2,
+        title:"food-item-1",
+        catagory:"food",
+        price:200,
+        img:"https://media.istockphoto.com/id/1169700146/photo/assorted-indian-non-vegetarian-food-recipe-served-in-a-group-includes-chicken-curry-mutton.webp?b=1&s=170667a&w=0&k=20&c=t6MdFXAoB475RALAXaJrVM-sKbUDblqMtPkeRwPDzzQ=",
+        desc:"This is food challange for the people who love it and cjkdjd kdj dkifk didjfj polorrdjjd fklkgk okgkg"
+    },
+    {
+        id:3,
+        title:"food-item-1",
+        catagory:"food",
+        price:200,
+        img:"https://media.istockphoto.com/id/1169700146/photo/assorted-indian-non-vegetarian-food-recipe-served-in-a-group-includes-chicken-curry-mutton.webp?b=1&s=170667a&w=0&k=20&c=t6MdFXAoB475RALAXaJrVM-sKbUDblqMtPkeRwPDzzQ=",
+        desc:"This is food challange for the people who love it and cjkdjd kdj dkifk didjfj polorrdjjd fklkgk okgkg"
+    },
+    {
+        id:4,
+        title:"food-item-1",
+        catagory:"food",
+        price:200,
+        img:"https://media.istockphoto.com/id/1169700146/photo/assorted-indian-non-vegetarian-food-recipe-served-in-a-group-includes-chicken-curry-mutton.webp?b=1&s=170667a&w=0&k=20&c=t6MdFXAoB475RALAXaJrVM-sKbUDblqMtPkeRwPDzzQ=",
+        desc:"This is food challange for the people who love it and cjkdjd kdj dkifk didjfj polorrdjjd fklkgk okgkg"
+    },
+    {
+        id:5,
+        title:"food-item-1",
+        catagory:"food",
+        price:200,
+        img:"https://media.istockphoto.com/id/1169700146/photo/assorted-indian-non-vegetarian-food-recipe-served-in-a-group-includes-chicken-curry-mutton.webp?b=1&s=170667a&w=0&k=20&c=t6MdFXAoB475RALAXaJrVM-sKbUDblqMtPkeRwPDzzQ=",
+        desc:"This is food challange for the people who love it and cjkdjd kdj dkifk didjfj polorrdjjd fklkgk okgkg"
+    },
+    {
+        id:6,
+        title:"food-item-1",
+        catagory:"food",
+        price:200,
+        img:"https://media.istockphoto.com/id/1169700146/photo/assorted-indian-non-vegetarian-food-recipe-served-in-a-group-includes-chicken-curry-mutton.webp?b=1&s=170667a&w=0&k=20&c=t6MdFXAoB475RALAXaJrVM-sKbUDblqMtPkeRwPDzzQ=",
+        desc:"This is food challange for the people who love it and cjkdjd kdj dkifk didjfj polorrdjjd fklkgk okgkg"
+    },
+    {
+        id:7,
+        title:"food-item-1",
+        catagory:"food",
+        price:200,
+        img:"https://media.istockphoto.com/id/1169700146/photo/assorted-indian-non-vegetarian-food-recipe-served-in-a-group-includes-chicken-curry-mutton.webp?b=1&s=170667a&w=0&k=20&c=t6MdFXAoB475RALAXaJrVM-sKbUDblqMtPkeRwPDzzQ=",
+        desc:"This is food challange for the people who love it and cjkdjd kdj dkifk didjfj polorrdjjd fklkgk okgkg"
+    },
+    {
+        id:8,
+        title:"food-item-1",
+        catagory:"food",
+        price:200,
+        img:"https://media.istockphoto.com/id/1169700146/photo/assorted-indian-non-vegetarian-food-recipe-served-in-a-group-includes-chicken-curry-mutton.webp?b=1&s=170667a&w=0&k=20&c=t6MdFXAoB475RALAXaJrVM-sKbUDblqMtPkeRwPDzzQ=",
+        desc:"This is food challange for the people who love it and cjkdjd kdj dkifk didjfj polorrdjjd fklkgk okgkg"
+    },
+    {
+        id:9,
+        title:"food-item-1",
+        catagory:"food",
+        price:200,
+        img:"https://media.istockphoto.com/id/1169700146/photo/assorted-indian-non-vegetarian-food-recipe-served-in-a-group-includes-chicken-curry-mutton.webp?b=1&s=170667a&w=0&k=20&c=t6MdFXAoB475RALAXaJrVM-sKbUDblqMtPkeRwPDzzQ=",
+        desc:"This is food challange for the people who love it and cjkdjd kdj dkifk didjfj polorrdjjd fklkgk okgkg"
+    },
+]
+
+export default item;
